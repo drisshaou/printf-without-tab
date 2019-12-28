c00338bd611b01c8fae07e9d2cd4535b22c693e7
driss haouhaou <driss@MacBook-Pro-de-driss-6.local>
2019-12-28 03:42:45 +0100

    ft_printf without tab

--- ft_convert_str.c
+++ ft_convert_str.c
@@ -6,7 +6,7 @@
 /*   By: dhaouhao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/24 01:33:09 by dhaouhao          #+#    #+#             */
-/*   Updated: 2019/12/27 06:23:25 by dhaouhao         ###   ########.fr       */
+/*   Updated: 2019/12/28 03:42:16 by dhaouhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ void	ft_convert_str(const char *str, char **converted_str, va_list args)
 
 	while (*str)
 	{
-		i = (str[0] == '%') ? 1 : 0;
+		i = (*str == '%') ? 1 : 0;
 		while (str[i] && ((*str != '%' && str[i] != '%') ||
 		(*str == '%' && !ft_is_type(str[i], TYPES))))
 			i++;
 		if (i > 0)
 		{
-			i += (*str == '%') ? 1 : 0;
-			if (*str == '%')
+			//i += (*str == '%') ? 1 : 0;
+			if (*str == '%' && i++)
 				string = ft_get_formated(ft_substr(str, 1, i - 1), args);
 			else
 				string = ft_substr(str, 0, i);
